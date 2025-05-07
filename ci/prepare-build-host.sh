eed183d0ade6eb0c7e684ce355b8c6f1a794ed5d
Alexander Shirokov <shirokovalexs@gmail.com>
2025-05-07 10:03:02 +0200

    ci:install genimage and DOS tools

--- ci/prepare-build-host.sh
+++ ci/prepare-build-host.sh
@@ -4,7 +4,7 @@
 apt update -y
 
 apt install -y gcc g++ make git zip curl wget sed binutils build-essential \
-  patch gzip bzip2 perl tar cpio unzip rsync file bc findutils
+  patch gzip bzip2 perl tar cpio unzip rsync file bc findutils genimage mtools dosfstools
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o install-rust.sh
