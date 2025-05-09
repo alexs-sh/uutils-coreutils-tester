e48fcda76e166ae2cada1f851c06d4ae0682d343
Alexander Shirokov <shirokovalexs@gmail.com>
2025-05-09 09:57:09 +0200

    build:multicall/non-multicall images

--- build-img.sh
+++ build-img.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 DEFCONFIG=qemu_aarch64_virt_uutils_coreutils_defconfig
+MODE="multicall"
 
 if [ "$#" -ge 1 ]; then
    DEFCONFIG=$1
 fi
 
+if [ "$#" -gt 1 ]; then
+   MODE=$2
+fi
+
+readonly BR_CONFIG=buildroot/configs/"$DEFCONFIG"
+
 set -e
 . "$HOME/.cargo/env"
 
 echo "Add config $DEFCONFIG"
-cp configs/"$DEFCONFIG" buildroot/configs/"$DEFCONFIG"
+cp configs/"$DEFCONFIG" "$BR_CONFIG"
+
+if [ "$MODE" == "non-multicall" ]; then
+  echo "Disable multicall"
+  echo "# BR2_PACKAGE_UUTILS_COREUTILS_MULTICALL is not set" >> "$BR_CONFIG"
+fi
 
 echo "Apply config $DEFCONFIG"
 cd buildroot
