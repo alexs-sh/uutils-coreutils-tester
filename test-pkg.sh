bea1214eed9130bd5c812ac0c1313bc77ef7d962
Alexander Shirokov <shirokovalexs@gmail.com>
2025-09-26 15:39:59 +0200

    add test-pkg support

---
+++ test-pkg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MODE=""
+CONFIG_NAME=uutils-coreutils
+CONFIG_FILE=${CONFIG_NAME}.config
+CMD="./utils/test-pkg -p uutils-coreutils  -c uutils-coreutils.config"
+
+if [ "$#" -gt 1 ]; then
+   MODE=$2
+fi
+
+
+set -e
+. "$HOME/.cargo/env"
+
+
+cp -f configs/$CONFIG_FILE buildroot/
+cd buildroot
+
+if [ "$MODE" = "all" ]; then
+  CMD+=" --all"
+fi
+
+export FORCE_UNSAFE_CONFIGURE=1
+eval "$CMD"
